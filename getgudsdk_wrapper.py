b203b5ecbae28d1a955383d11ddf554cbce3081d
artkulak <artkulak444@gmail.com>
2024-01-15 11:24:17 +0000

    Update sdk to latest version + add support for updated SDK + add affect action

--- getgudsdk_wrapper.py
+++ getgudsdk_wrapper.py
@@ -1,12 +1,19 @@
 import getgudsdk.lib as getgudsdk
 from getgudsdk import ffi
 import time
+from enum import Enum
 
 class GetGudSdk:
+    class AffectState(Enum):
+        Attach = 0
+        Activate = 1
+        Deactivate = 2
+        Detach = 3
+        
     def __init__(self):
         getgudsdk.init()
 
-    def start_game(self, title_id, private_key, server_guid, game_mode):
+    def start_game(self, title_id, private_key, server_guid, game_mode, server_location):
         game_info = ffi.new("struct StartGameInfo*")
         game_info.titleId = title_id
 
@@ -21,6 +28,11 @@ class GetGudSdk:
         game_mode_data = ffi.new("char[]", game_mode.encode('utf-8'))
         game_info.gameMode = game_mode_data
         game_info.gameModeSize = len(game_mode)
+        
+        
+        server_location_data = ffi.new("char[]", server_location.encode('utf-8'))
+        game_info.serverLocation = server_location_data
+        game_info.serverLocationSize = len(server_location)
 
         game_guid = ffi.new("char[256]") 
         getgudsdk.StartGame(game_info[0], game_guid)
@@ -170,7 +182,7 @@ class GetGudSdk:
 
 
     
-    def send_death_action(self, match_guid, action_time_epoch, player_guid):
+    def send_death_action(self, match_guid, action_time_epoch, player_guid, attacker_guid):
         base_data = ffi.new("struct BaseActionData*")
         base_data.actionTimeEpoch = action_time_epoch
         match_guid_data = ffi.new("char[]", match_guid.encode('utf-8'))
@@ -179,8 +191,11 @@ class GetGudSdk:
         player_guid_data = ffi.new("char[]", player_guid.encode('utf-8'))
         base_data.playerGuid = player_guid_data
         base_data.playerGuidSize = len(player_guid)
+        
+        attacker_guid_data = ffi.new("char[]", attacker_guid.encode('utf-8'))
+        attacker_guid_size = len(attacker_guid)
 
-        return getgudsdk.SendDeathAction(base_data[0])
+        return getgudsdk.SendDeathAction(base_data[0], attacker_guid_data, attacker_guid_size)
 
     def send_position_action(self, match_guid, action_time_epoch, player_guid, position, rotation):
         base_data = ffi.new("struct BaseActionData*")
@@ -231,8 +246,52 @@ class GetGudSdk:
 
         return result_code
     
-    def update_player(self, title_id, private_key, player_guid, player_nickname, player_email,
-                        player_rank, player_join_date_epoch):
+    def send_affect_action(self, match_guid, action_time_epoch, player_guid, affect_guid, affect_state):
+        base_data = ffi.new("struct BaseActionData*")
+        base_data.actionTimeEpoch = action_time_epoch
+
+        match_guid_data = ffi.new("char[]", match_guid.encode('utf-8'))
+        base_data.matchGuid = match_guid_data
+        base_data.matchGuidSize = len(match_guid)
+
+        player_guid_data = ffi.new("char[]", player_guid.encode('utf-8'))
+        base_data.playerGuid = player_guid_data
+        base_data.playerGuidSize = len(player_guid)
+
+        affect_guid_data = ffi.new("char[]", affect_guid.encode('utf-8'))
+        affect_guid_size = len(affect_guid)
+        
+        # Check if affect_state is an instance of the AffectState enum or a string
+        if isinstance(affect_state, str):
+            affect_state_value = self.AffectState[affect_state].value
+        else:
+            affect_state_value = affect_state.value
+
+        result_code = getgudsdk.SendAffectAction(base_data[0], affect_guid_data, affect_guid_size, affect_state_value)
+
+        return result_code
+     
+    def update_player(
+        self, 
+        title_id, 
+        private_key, 
+        player_guid, 
+        player_nickname, 
+        player_email,
+        player_rank, 
+        player_join_date_epoch,
+        player_suspect_score, 
+        player_reputation,           
+        player_status, 
+        player_campaign, 
+        player_notes, 
+        player_device, 
+        player_os, 
+        player_age, 
+        player_gender, 
+        player_location, 
+        transactions
+    ):
         
         private_key_data = ffi.new("char[]", private_key.encode('utf-8'))
         privateKeySize = len(private_key)
@@ -253,6 +312,61 @@ class GetGudSdk:
 
         player_info.playerRank = player_rank
         player_info.playerJoinDateEpoch = player_join_date_epoch
+        
+        player_suspect_score_data = ffi.new("char[]", player_suspect_score.encode('utf-8'))
+        player_info.playerSuspectScore = player_suspect_score_data
+        player_info.playerSuspectScoreSize = len(player_suspect_score)
+
+        player_reputation_data = ffi.new("char[]", player_reputation.encode('utf-8'))
+        player_info.playerReputation = player_reputation_data
+        player_info.playerReputationSize = len(player_reputation)
+
+        player_status_data = ffi.new("char[]", player_status.encode('utf-8'))
+        player_info.playerStatus = player_status_data
+        player_info.playerStatusSize = len(player_status)
+
+        player_campaign_data = ffi.new("char[]", player_campaign.encode('utf-8'))
+        player_info.PlayerCampaign = player_campaign_data
+        player_info.PlayerCampaignSize = len(player_campaign)
+
+        player_notes_data = ffi.new("char[]", player_notes.encode('utf-8'))
+        player_info.playerNotes = player_notes_data
+        player_info.playerNotesSize = len(player_notes)
+
+        player_device_data = ffi.new("char[]", player_device.encode('utf-8'))
+        player_info.playerDevice = player_device_data
+        player_info.playerDeviceSize = len(player_device)
+
+        player_os_data = ffi.new("char[]", player_os.encode('utf-8'))
+        player_info.playerOS = player_os_data
+        player_info.playerOSSize = len(player_os)
+
+        player_info.playerAge = player_age
+
+        player_gender_data = ffi.new("char[]", player_gender.encode('utf-8'))
+        player_info.playerGender = player_gender_data
+        player_info.playerGenderSize = len(player_gender)
+
+        player_location_data = ffi.new("char[]", player_location.encode('utf-8'))
+        player_info.playerLocation = player_location_data
+        player_info.playerLocationSize = len(player_location)
+
+        # Handle transactions if provided
+        if transactions:
+            # Assuming transactions is a list of dictionaries
+            transaction_structs = [ffi.new("struct PlayerTransactions*", {
+                'TransactionGuid': ffi.new("char[]", transaction['TransactionGuid'].encode('utf-8')),
+                'TransactionGuidSize': len(transaction['TransactionGuid']),
+                'TransactionName': ffi.new("char[]", transaction['TransactionName'].encode('utf-8')),
+                'TransactionNameSize': len(transaction['TransactionName']),
+                'TransactionDateEpoch': transaction['TransactionDateEpoch'],
+                'TransactionValueUSD': transaction['TransactionValueUSD']
+            }) for transaction in transactions]
+            player_info.transactions = ffi.new("struct PlayerTransactions[]", transaction_structs)
+            player_info.transactionsSize = len(transactions)
+        else:
+            player_info.transactions = ffi.NULL
+            player_info.transactionsSize = 0
         
         result_code = getgudsdk.UpdatePlayer(title_id, private_key_data, privateKeySize, player_info[0])
 
